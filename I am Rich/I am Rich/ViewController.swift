9bf69a40a68224b9e6c3bad1a0c97f865d72e46c
paniut <111059734+paniut@users.noreply.github.com>
2023-06-25 19:07:23 +0200

    Add files via upload

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Szczepan Podulka on 17/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
